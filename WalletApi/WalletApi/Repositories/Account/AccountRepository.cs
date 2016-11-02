ba09cd061d2b4bfc3326f04dcd4bb9191fdd1169
nicola.atorino <nicola.atorino@igamingcloud.com>
2016-11-02 20:37:26 +0100

    added account repository + fixed typo

---
+++ WalletApi/WalletApi/Repositories/Account/AccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalletApi.DataLayer;
+using WalletApi.Repositories.Generic;
+
+namespace WalletApi.Repositories.Account
+{
+    public class AccountRepository : 
+        AbstractRepository<DataLayer.Account>, IAccountRepository
+    {
+       //all interfaces methods are implemented by base class.
+    }
+}
